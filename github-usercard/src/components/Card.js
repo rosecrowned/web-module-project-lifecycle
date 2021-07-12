da8954eb47c142248132dff93ad9296cbd6f3d98
unknown <seivey@hotmail.com>
2021-07-11 21:26:42 -0600

    basic outline of app setup

---
+++ github-usercard/src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Details from './Details'
+
+class Card extends React.Component {
+
+    componentDidMount() {
+
+    }
+
+    componentDidUpdate(){
+
+    }
+
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default Card;
---
+++ github-usercard/src/components/Details.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Details extends React.Component {
+
+    render(){
+        return(
+
+        )
+    }
+}
+
+export default Details
